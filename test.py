56b6d031549a04eadc0e6708355a1756057362dc
DebaA17 <debashis.2520017@bppimt.com>
2025-08-09 21:02:48 +0530

    initial commit

--- test.py
+++ test.py
@@ -1,14 +1,45 @@
-# This is a test file
+# # This is a test file
+#
+# n = int(input("Enter the element : "))
+#
+# arr =[]
+#
+# print("Enter the elements : ")
+# for i in range(n):
+#     arr.append(int(input()))
+#
+# total = sum(arr)
+#
+# print(f"Array : {arr}")
+# print(f"The sum of array : {total}")
 
-n = int(input("Enter the element : "))
+info = {
+    "key": "value",
+    "name": "Anonymous",
+    "age": 69,
+    "mark": 69,
+    "college": "BPPIMT",
+    "classmates": ["alice", "bob"]
 
-arr =[]
+}
 
-print("Enter the elements : ")
-for i in range(n):
-    arr.append(int(input()))
+student = {
+    "name": "Debasis",
+    "subjects": {
+        "math":95,
+        "science":90,
+        "english":69,
+    },
 
-total = sum(arr)
+    "session": "2025-2026",
+}
+student.update({"city" : "Bengaluru"})
 
-print(f"Array : {arr}")
-print(f"The sum of array : {total}")
+print(student['subjects'] ["english"])
+print(student["city"])
+
+
+
+print(info)
+print(type(info))
+print(info["age"])
