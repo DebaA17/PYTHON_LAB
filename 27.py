8508b77b50032b8cbdd80f271f9ef32443196c6b
DebaA17 <debashis.2520017@bppimt.com>
2025-08-11 22:13:55 +0530

    Initial commit

---
+++ 27.py
@@ -0,0 +1,5 @@
+lst = input("Enter list elements (space-separated): ").split()
+i, j = map(int, input("Enter two indices to swap: ").split())
+lst[i], lst[j] = lst[j], lst[i]
+print(lst)
+#test file 
