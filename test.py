a18d1a5fdcb0d5700d7bebd4373bd41f40d05287
DebaA17 <debashis.2520017@bppimt.com>
2025-08-11 22:13:35 +0530

    Initial commit

--- test.py
+++ test.py
@@ -1,45 +1,4 @@
-# # This is a test file
-#
-# n = int(input("Enter the element : "))
-#
-# arr =[]
-#
-# print("Enter the elements : ")
-# for i in range(n):
-#     arr.append(int(input()))
-#
-# total = sum(arr)
-#
-# print(f"Array : {arr}")
-# print(f"The sum of array : {total}")
+times = int(input("Enter the value : "))
 
-info = {
-    "key": "value",
-    "name": "Anonymous",
-    "age": 69,
-    "mark": 69,
-    "college": "BPPIMT",
-    "classmates": ["alice", "bob"]
-
-}
-
-student = {
-    "name": "Debasis",
-    "subjects": {
-        "math":95,
-        "science":90,
-        "english":69,
-    },
-
-    "session": "2025-2026",
-}
-student.update({"city" : "Bengaluru"})
-
-print(student['subjects'] ["english"])
-print(student["city"])
-
-
-
-print(info)
-print(type(info))
-print(info["age"])
+for i in range(1, times + 1):
+    print(f"{i}")
