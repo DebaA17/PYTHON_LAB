02e758b075942159272c13e9197818afb9112546
DebaA17 <debashis.2520017@bppimt.com>
2025-08-11 22:21:18 +0530

    Initial commit

---
+++ 29.py
@@ -0,0 +1,5 @@
+# Python program to reverse a list
+# This code may needs to change in future according to faculty instructions
+
+lst = input("Enter list elements (space-separated): ").split()
+print(lst[::-1])
